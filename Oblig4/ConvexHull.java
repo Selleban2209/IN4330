9094a5d4242c1283af3d1570680de537ff533629
salleban farah <salle2209@gmail.com>
2024-04-04 14:14:01 +0200

    start of oblig4

---
+++ Oblig4/ConvexHull.java
@@ -0,0 +1,14 @@
+
+/**
+ * A basic class to allow the precode to compile. You will need to implement the
+ * logic for finding what points make up the convex hull.
+ * 
+ */
+public class ConvexHull {
+    int n, MAX_X, MAX_Y;
+    int x[], y[];
+
+    ConvexHull() {
+    }
+
+}
